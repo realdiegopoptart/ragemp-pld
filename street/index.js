e11556496491dc64b1abc97d9b43971f961af5cb
Diego Casillas <52338302+realdiegopoptart@users.noreply.github.com>
2022-12-03 09:59:34 -0700

    Add files via upload

---
+++ street/index.js
@@ -0,0 +1,47 @@
+﻿let street = mp.browsers.new("package://street/index.html");
+let player = mp.players.local;
+const updateInterval = 500; // milliseconds, lower value = more accurate, at the cost of performance
+
+let getStreet;
+let streetName;
+let crossingName;
+let zoneName;
+let direction;
+
+setInterval(() => {
+    getStreet = mp.game.pathfind.getStreetNameAtCoord(player.position.x, player.position.y, player.position.z, 0, 0);
+    // Returns obj {"streetName": hash, crossingRoad: hash}
+
+    streetName = mp.game.ui.getStreetNameFromHashKey(getStreet.streetName);
+    crossingName = mp.game.ui.getStreetNameFromHashKey(getStreet.crossingRoad);
+    // Returns string, if exist
+
+    zoneName = mp.game.gxt.get(mp.game.zone.getNameOfZone(player.position.x, player.position.y, player.position.z));
+
+    let heading = player.getHeading();
+    if(player.heading != 0)
+    {
+        if(heading < 45 || heading > 315)
+        {
+            direction = "N";
+        }
+        if(heading > 45 && heading < 135)
+        {
+            direction = "W";
+        }
+        if(heading > 135 && heading < 225)
+        {
+            direction = "S";
+        }
+        if(heading > 225 && heading < 315)
+        {
+            direction = "E";
+        }
+    }
+}, updateInterval);
+
+mp.events.add('render', () => {
+    let crossing;
+    if(crossingName != "") { crossing = `/ ${crossingName}`; } else { crossing = ""; }
+    street.execute(`update('${streetName}', '${crossing}', '${zoneName}', '${direction}');`);
+});
